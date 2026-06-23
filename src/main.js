b567e723f1ce80e4fba4ef886eb3150e60715721
Daniel Rairan <daniel.rairan@omc.com>
2026-06-23 15:52:49 -0500

    feat(js): add paso99.js for string manipulation concepts and update main imports

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import './lecciones/paso68.js';
 import './lecciones/paso69.js';
 import './lecciones/paso86.js';
 import './lecciones/paso87.js';
+import './lecciones/paso99.js';
 import './talleres/taller1.js';
 import './talleres/taller2.js';
